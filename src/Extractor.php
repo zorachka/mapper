fa6b83fc20356659341ad82dd672a32f04bd33df
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-07-15 13:35:06 +0400

    feat: add Extractor

---
+++ src/Extractor.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zorachka\Mapper;
+
+use ReflectionClass;
+use ReflectionException;
+
+final class Extractor
+{
+    /**
+     * @var array<string, ReflectionClass>
+     */
+    private array $reflectionClassMap;
+
+    /**
+     * @param object $object
+     * @param array $fields
+     * @return array<string, mixed>
+     * @throws ReflectionException
+     */
+    public function extract(object $object, array $fields): array
+    {
+        $reflection = $this->getReflectionClassInstance(get_class($object));
+        $result = [];
+
+        foreach ($fields as $name) {
+            $property = $reflection->getProperty($name);
+            if ($property->isPrivate() || $property->isProtected()) {
+                $property->setAccessible(true);
+            }
+            $result[$property->getName()] = $property->getValue($object);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $className
+     * @return ReflectionClass
+     * @throws ReflectionException
+     */
+    private function getReflectionClassInstance(string $className): ReflectionClass
+    {
+        if (!isset($this->reflectionClassMap[$className])) {
+            $this->reflectionClassMap[$className] = new ReflectionClass($className);
+        }
+
+        return $this->reflectionClassMap[$className];
+    }
+}
---
+++ tests/ExtractorTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zorachka\Mapper\Tests;
+
+use ReflectionException;
+use PHPUnit\Framework\TestCase;
+use Zorachka\Mapper\Extractor;
+
+final class ExtractorTest extends TestCase
+{
+    private Extractor $extractor;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->extractor = new Extractor();
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testFull()
+    {
+        $testObject = new TestClass(1, 2, 3);
+
+        $row = $this->extractor->extract($testObject, ['publicField', 'protectedField', 'privateField']);
+
+        self::assertEquals([
+            'publicField' => $testObject->publicField,
+            'protectedField' => $testObject->getProtectedField(),
+            'privateField' => $testObject->getPrivateField(),
+        ], $row);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testPartial()
+    {
+        $testObject = new TestClass(1, 2, 3);
+        $row = $this->extractor->extract($testObject, ['protectedField', 'privateField']);
+
+        self::assertEquals([
+            'protectedField' => $testObject->getProtectedField(),
+            'privateField' => $testObject->getPrivateField(),
+        ], $row);
+    }
+
+    public function testNotExist()
+    {
+        self::expectException(ReflectionException::class);
+
+        $testObject = new TestClass(1, 2, 3);
+        $this->extractor->extract($testObject, ['publicField', 'otherField']);
+    }
+}
